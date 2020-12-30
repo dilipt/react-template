61606bce0e71b7868ce4d9916fa994e1bacf461b
dilipt <dthomas84@gmail.com>
2020-12-30 19:16:21 +1100

    audit fix, use facebook-style components and linting.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const App = () => (
-  <>
-    <header>
-      <h1>Frontend Template</h1>
-    </header>
-    <main>
-      Feel free to add some content here.
-    </main>
-  </>
-);
-
-export default App;
+export function App() {
+  return (
+    <>
+      <header>
+        <h1>Frontend Template</h1>
+      </header>
+      <main>
+        Feel free to add some content here.
+      </main>
+    </>
+  );
+}
