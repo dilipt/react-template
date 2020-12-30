23ff2394b86003d8e4752873b8933e93b1a50513
dilipt <dthomas84@gmail.com>
2020-12-30 19:25:23 +1100

    add basic styled-component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & > *:not(:last-child) {
+    margin-bottom: 20px;
+  }
+`;
 
 export function App() {
   return (
-    <>
+    <Container>
       <header>
         <h1>Frontend Template</h1>
       </header>
-      <main>
+      <article>
         Feel free to add some content here.
-      </main>
-    </>
+      </article>
+    </Container>
   );
 }
