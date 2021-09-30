22a97f3437631675917468686ab87f611e245a88
Dilip Thomas <dthomas84@gmail.com>
2021-10-01 08:49:01 +1000

    gets eslint working with typescript.

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/extensions': [0, {}],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.tsx', '**/*.test.ts'] }],
+  },
+  settings: {
+    'import/resolver': 'webpack',
+  },
+};
